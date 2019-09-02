46b0a820566b6447d1019d17d128c140957b657e
milano12345 <dmilano857@gmail.com>
2019-09-01 21:27:18 -0500

    completed TodoData, TodoList, Todo pages

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,13 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+import TodoData from './TodoData';
+import Todo from './Todo';
+
+function TodoList() {
+    return (
+    TodoData.map(item => <Todo key={item.id} todo={item}/>)
+    )
+}
+
+export default TodoList;
